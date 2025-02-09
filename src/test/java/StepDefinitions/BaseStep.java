6db4a6d7ab445f201bd50cb9d50dd255bd744feb
Saranyavasu <vprsaran@gmail.com>
2025-02-09 13:07:57 +0530

    New parabank project

---
+++ src/test/java/StepDefinitions/BaseStep.java
@@ -0,0 +1,21 @@
+package StepDefinitions;
+
+
+import org.openqa.selenium.WebDriver;
+
+public class BaseStep {
+
+public static WebDriver driver;
+//    public LoginPage lp= new LoginPage();
+//
+//    @Before
+//    public void Before() {
+//        //add code to pick from browserfactory
+//        WebDriver driver = BrowserFactory.InitateBrowser();
+//        driver.manage().window().maximize();
+//        driver.get(PropertiesUtils.getproperty("url"));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+//
+//    }
+
+}
